9a56444435fbcbbf9c4a92b8d200ce3102733a56
Sun <a24230928@gmail.com>
2021-10-24 22:08:57 +0800

    problem 35

---
+++ Easy/35_search_insert_position.py
@@ -0,0 +1,20 @@
+"""
+related topic: array, binary search
+time complexity: O(log(n))
+space complexity: O(1)
+
+"""
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
